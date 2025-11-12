dc3049f66d4e694664b28d8cce9f3d3d23ec30b2
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-11-12 09:47:23 +0200

    initial setup

---
+++ app/tic-tac-toe/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Square() {
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
+}
+
+
