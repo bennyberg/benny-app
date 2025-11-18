a6b3f0f5e3cfcebca0bc6f63d7074b094c1bf3d9
benjaminberg <benjamin.berg@cs.huji.ac.il>
2025-11-18 14:38:16 +0200

    finished designing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,45 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main id={styles["homepage"]}>
-          <div className={styles["logo-section"]}>
-          <Image
-            src="/huji.svg"
-            alt="HUJI Logo"
-            width="80"
-            height="80"
-            priority
-          />
-          <Image
-            src="/bezalel.svg"
-            alt="Bezalel Logo"
-            className="item"
-            width="80"
-            height="80"
-            priority
-          />
-        </div>
+      <div className={styles["logo-section"]}>
+        <Image
+          src="/huji.svg"
+          alt="HUJI Logo"
+          width="80"
+          height="80"
+          priority
+        />
+        <Image
+          src="/bezalel.svg"
+          alt="Bezalel Logo"
+          className="item"
+          width="80"
+          height="80"
+          priority
+        />
+      </div>
 
       <div>
-        <h2>Welcome to my homepage</h2>
-        <p>Check out my projects!</p>
+        <h2 className={styles["subheader"]}>Welcome to my homepage</h2>
+        <p className={styles["main-paragraph"]}>Check out my projects!</p>
         <div>
-          <h2>Links</h2>
+          <h2 className={styles["subheader"]}>Links</h2>
           <div className={styles["link-section"]}>
-            <Link href={"/tic-tac-toe"} className={styles["link-button"]}>Tic-Tac-Toe</Link>
-            <Link href={"/art"} className={styles["link-button"]}>Met Museum of Art</Link>
-            <Link href={"/design"} className={styles["link-button"]}>Design to Code</Link>
+            <div className={styles["link-container"]}>
+              <Link href={"/tic-tac-toe"} className={styles["link-button"]}>
+                Tic-Tac-Toe
+              </Link>
+              <Link href={"/art"} className={styles["link-button"]}>
+                Met Museum of Art
+              </Link>
+              <Link href={"/design"} className={styles["link-button"]}>
+                Design to Code
+              </Link>
+            </div>
           </div>
         </div>
       </div>
